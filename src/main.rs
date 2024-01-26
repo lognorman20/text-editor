3853ed3d103fd6da9d9babdb584247d90ef9c984
Logan Norman <lognorman20@gmail.com>
2024-01-25 19:24:09 -0700

    add vim like quitting, start of ch3

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for byte in io::stdin().bytes() {
-        let b = byte.unwrap();
-        let c = b as char;
+    let mut prev_key: Key = Key::Char('_');
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?} \r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
 
-        if c.is_control() {
-            println!("{:?} is a goofy character\r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
-    
-        if c == 'q' {
-            break;
+                    // quit the program vim style
+                    if c == 'q' && prev_key == Key::Char(':') {
+                        break;
+                    }
+
+                    prev_key = key;
+                }
+
+                // handle all non ascii characters (backspace, ctrl, etc...)
+                _ => println!("{:?} goofy key\r", key),
+            },
+            Err(err) => {
+                die(err);
+            }
         }
     }
 }
