d12dea80b548ca09b9e0ddca564545d0d2e55069
Logan Norman <lognorman20@gmail.com>
2024-01-25 21:09:47 -0700

    add welcome msg, nice init

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 use crate::Terminal;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     terminal: Terminal,
     should_quit: bool,
@@ -31,19 +33,39 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        Terminal::clear_screen();
+        Terminal::cursor_hide();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
+            Terminal::clear_screen();
             println!("SAY GOODBYE \r");
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1,1));
+            print!("{}", termion::cursor::Goto(1, 1));
         }
+        Terminal::cursor_show();
         Terminal::flush()
     }
 
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.get_size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
+
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.get_size().height {
-            println!("~\r");
+        let height = self.terminal.get_size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
     }
 
